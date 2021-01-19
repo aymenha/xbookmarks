8eadd58677ed90eded23c5346cbafd04b2c2942b
Amine <mouhamedaymenhammami@gmail.com>
2021-01-19 01:11:29 +0100

    👌 IMPROVE: faster build/deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
-  assetPrefix: isProd ? "/bookmarks" : "",
-  basePath: isProd ? "/bookmarks" : "",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
 };
