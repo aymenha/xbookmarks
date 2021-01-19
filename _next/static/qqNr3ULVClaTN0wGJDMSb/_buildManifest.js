208e03b4286e4af34b2505b7d47e171efc086873
aymenha <aymenha@users.noreply.github.com>
2021-01-19 01:03:08 +0000

    deploy: e6bda84a2cffe003d402661de9d0cbad6b7d629b

---
+++ _next/static/qqNr3ULVClaTN0wGJDMSb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-f64e30464bee62163657.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ebd436dee5954f3dcb1b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
